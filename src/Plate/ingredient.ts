363d6f51a448b418ee420b3bce3a1cc9981086eb
Miguel Martín <alu0101209777@ull.edu.es>
2021-04-03 21:03:25 +0100

    Ingredient class added and tested

---
+++ src/Plate/ingredient.ts
@@ -0,0 +1,25 @@
+import { BasicFood, Macronutrients } from "../Food";
+
+
+export class Ingredient {
+  constructor(private food: BasicFood, private ammount: number) {
+
+  }
+
+  /**
+   * 
+   * @returns Precio del ingrediente
+   */
+  getPrice(): number {
+    return this.food.getPriceByKg() * (this.ammount / 1000);
+  }
+
+  getNutritionalComposition(): Macronutrients {
+    const nutritionalComposition = new Macronutrients();
+    nutritionalComposition.lipids = this.food.getMacronutrients().lipids * (this.ammount / 100);
+    nutritionalComposition.carbohydrates = this.food.getMacronutrients().carbohydrates * (this.ammount / 100);
+    nutritionalComposition.proteins = this.food.getMacronutrients().proteins * (this.ammount / 100);
+
+    return nutritionalComposition;
+  }
+};
