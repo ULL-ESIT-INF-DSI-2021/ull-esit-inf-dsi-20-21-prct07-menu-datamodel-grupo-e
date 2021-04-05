803196bf6d961020634082f273580a829f2651bf
Miguel Martín <alu0101209777@ull.edu.es>
2021-04-05 09:16:46 +0100

    Cereal added to Food/index.ts

--- src/Food/index.ts
+++ src/Food/index.ts
@@ -3,4 +3,5 @@ export { Macronutrients } from './macronutrients';
 export { RichProteinFood } from './rich_protein_food';
 export { Meat } from './meat';
 export { Vegetable } from './vegetable';
+export { Cereal } from './cereal';
 
