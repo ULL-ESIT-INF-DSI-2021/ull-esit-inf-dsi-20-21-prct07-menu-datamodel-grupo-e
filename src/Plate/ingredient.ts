5fb057c54c4e3b6fdf916c917b91959fe876f495
Yago Pérez Molanes <alu0101254678@ull.edu.es>
2021-04-07 18:27:22 +0100

    Added documentation to codde /src/Plate

--- src/Plate/ingredient.ts
+++ src/Plate/ingredient.ts
@@ -1,13 +1,26 @@
 import { BasicFood, Macronutrients } from "../Food";
 
 
+/**
+ * Clase ingrediente, hace de intermediario,
+ * entre BasicFood y BasicPlate
+ */
 export class Ingredient {
   private nutritionalComposition: Macronutrients;
 
+  /**
+   * Constructor de la clase Ingredient
+   * @param food un alimento (BasicFood)
+   * @param ammount cantidad del alimento
+   */
   constructor(private food: BasicFood, private ammount: number) {
     this.nutritionalComposition = this.calculateNutritionalComposition();
   }
 
+  /**
+   * Calcula la composición nutricional del alimento
+   * @returns objeto de tipo 'Macronutrients'
+   */
   private calculateNutritionalComposition(): Macronutrients {
     const nutritionalComposition = new Macronutrients();
     nutritionalComposition.lipids = this.food.getMacronutrients().lipids * (this.ammount / 100);
@@ -17,22 +30,34 @@ export class Ingredient {
     return nutritionalComposition;
   }
 
+  /**
+   * Getter del atributo 'food'
+   * @returns el objeto de BasicFood
+   */
   getFood(): BasicFood {
     return this.food;
   }
 
+  /**
+   * Getter del atributo 'amount'
+   * @returns la cantidad del ingrediente
+   */
   getAmmount(): number {
     return this.ammount;
   }
 
   /**
-   * 
-   * @returns Precio del ingrediente
+   * Getter del precio en gramos
+   * @returns el precio del ingrediente
    */
   getPrice(): number {
     return this.food.getPriceByKg() * (this.ammount / 1000);
   }
 
+  /**
+   * Getter del atributo 'nutritionalComposition'
+   * @returns composicion nutricional, objeto
+   */
   getNutritionalComposition(): Macronutrients {
     return this.nutritionalComposition;
   }
--- src/Plate/second_plate.ts
+++ src/Plate/second_plate.ts
@@ -1,11 +1,24 @@
 import { BasicPlate, PlateType } from "./basic_plate";
 import { Ingredient } from "./ingredient";
 
+/**
+ * Clase SecondPlate que extiende a BasicPlate, representa 
+ * a un objeto (segundo plato) de los platos posibles
+ */
 export class SecondPlate extends BasicPlate {
+  /**
+   * Constructor de la clase SecondPlate
+   * @param name Nombre del segundo plato
+   * @param ingredients Ingredientes (vector)
+   */
   constructor(name: string, ...ingredients: Ingredient[]) {
     super(name, ingredients);
   }
 
+  /**
+   * Obtiene el tipo de plato al que pertenece
+   * @returns un objeto enumerable PlateType
+   */
   getType() {
     return PlateType.secondPlate;
   }
