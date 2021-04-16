559496bf25527d94cedfd8f2fecd81450a9e42c6
Yago Pérez Molanes <alu0101254678@ull.edu.es>
2021-04-16 18:36:17 +0100

    Added documentation for Carta, Command, Menu and Stock

--- src/Carta/carta.ts
+++ src/Carta/carta.ts
@@ -1,19 +1,43 @@
 import { Menu } from "../Menu";
 import { BasicPlate } from "../Plate";
 
+/**
+ * Clase carta que permite manipular una serie de menús,
+ * añadir platos, indivuduales, menús prediseñados...
+ */
 export class Carta {
+  /**
+   * El constructor se fundamenta en los menús, y en los
+   * platos, y tiene un atributo que lo identifica
+   * @param name El nombre de la carta
+   * @param menus Un conjunto de menús
+   * @param singlePlates Un conjunto de platos individuales
+   */
   constructor(private name: string, private menus: Menu[], private singlePlates: BasicPlate[]) {
 
   }
 
+  /**
+   * Getter del atributo nombre
+   * @returns Una cadena de caracteres
+   */
   getName(): string {
     return this.name;
   }
   
+  /**
+   * Getter del atributo menus
+   * @returns Un vector de tipo Menu
+   */
   getMenus(): Menu[] {
     return this.menus;
   }
 
+  /**
+   * Getter del atributo singlePlates, añade los platos
+   * individuales internamente
+   * @returns Un vector de tipo BasicPlate
+   */
   getAllPlates(): BasicPlate[] {
     const usedPlates = new Set<string>();
     const allPlates: BasicPlate[] = this.singlePlates;
--- src/Command/command_order.ts
+++ src/Command/command_order.ts
@@ -1,16 +1,32 @@
 import { Menu } from "../Menu";
 import { BasicPlate } from "../Plate";
 
+/**
+ * Clase CommandOrder que gestiona un menú
+ * o plato, y una cantidad
+ */
 export class CommandOrder {
+  /**
+   * Constructor de la clase CommandOrder
+   * @param order Objeto de tipo Menu o BasicPlate
+   * @param quantity Cantidad numérica
+   */
   constructor(private order: Menu | BasicPlate, private quantity: number) {
     
   }
 
-
+  /**
+   * Getter del atributo order
+   * @returns Un Menú o un BasicPlate
+   */
   getOrder(): Menu | BasicPlate {
     return this.order;
   }
 
+  /**
+   * Getter del atributo quantity
+   * @returns Un valor numérico
+   */
   getQuantity(): number {
     return this.quantity;
   }
